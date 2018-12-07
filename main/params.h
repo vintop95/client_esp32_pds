44b43024bb94dd51ab3a05dca82cf9bfd1e75400
vintop95 <vincenzotop95@gmail.com>
2018-12-07 01:00:13 +0100

    change server addr

--- main/params.h
+++ main/params.h
@@ -9,7 +9,7 @@
     #define PARAMS_H_
 
     #define DEVICE_NAME "ESP0" 
-    #define CONF_CHOSEN 3
+    #define CONF_CHOSEN 1
 
     #if CONF_CHOSEN==-1
         #define WIFI_SSID "J8YYtd3"
@@ -20,13 +20,13 @@
         #define WIFI_PASS "ciao1234"
         #define SERVER_IP "192.168.43.5"
     #elif CONF_CHOSEN==1
-        // #define WIFI_SSID "PCDITOTÒ 7614"
-        // #define WIFI_PASS "Padrepio"
-        // #define SERVER_IP "192.168.137.1"
+        #define WIFI_SSID "PCDITOTÒ 7614"
+        #define WIFI_PASS "Padrepio"
+        #define SERVER_IP "192.168.137.1"
         /////
-        #define WIFI_SSID "xd"
-        #define WIFI_PASS "ciao1234"
-        #define SERVER_IP "192.168.43.211"
+        // #define WIFI_SSID "xd"
+        // #define WIFI_PASS "ciao1234"
+        // #define SERVER_IP "192.168.43.211"
     #elif CONF_CHOSEN==2
         #define WIFI_SSID "My ASUS G"
         #define WIFI_PASS "gianluca"
